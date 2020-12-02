4c5da1af31e28f71245893f16a1214f979132662
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-02 13:43:44 +0900

    add end_user_controller and homes_top_views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+  	admin_homes_top_path
+  end
+
+  def after_sign_out_path_for(resource)
+  	new_admin_seesion_path
+  end
 end
