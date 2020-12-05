8a8346cb509cb95bdac22653cc8bc205f137aa13
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-05 18:38:27 +0900

    add new to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   attachment :img
+
+  validates :name, presence: true, length: {maximum: 15}
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
+  validates :explanation, length: {maximum: 500}
+
+  def status_text
+  	if status == true
+  		"販売中"
+  	else
+  		"販売中止"
+  	end
+  end
 end
