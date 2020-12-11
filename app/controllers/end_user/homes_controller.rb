53b2784dfa3d62bd29dd548cb62298b370ff3cd3
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-11 11:35:45 +0900

    add course to EndUser_top

--- app/controllers/end_user/homes_controller.rb
+++ app/controllers/end_user/homes_controller.rb
@@ -2,6 +2,7 @@ class EndUser::HomesController < ApplicationController
   def top
   	items = Item.where(status: true).joins(:genre).where(genres: { is_active: true })
   	@items_rand = items.sample(3)
-  	@courses = Course.where(status: true).joins(:genre).where(genres: { is_active: true })
+  	courses = Course.where(status: true).joins(:genre).where(genres: { is_active: true })
+  	@courses_rand = courses.sample(3)
   end
 end
