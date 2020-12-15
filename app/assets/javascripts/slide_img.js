2201efd5dd81ec5aa1112b4557981441af980609
shofujimoto0617 <shofujimoto017@gmail.com>
2020-12-15 15:32:58 +0900

    add layout to EndUser_course_show

--- app/assets/javascripts/slide_img.js
+++ app/assets/javascripts/slide_img.js
@@ -1,10 +1,34 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
+
+
+  $(function() {
     $('.couresImg').slick({
         dots: true,
         autoplay: true,
         autoplaySpeed: 5000,
     });
+
+    $(".swiper-wrapper").slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        speed: 800,
+        dots: true,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20%'
+    });
+  });
 });
+
+
+
+// $(window).load(function() {
+//     $('.couresImg').slick({
+//         dots: true,
+//         autoplay: true,
+//         autoplaySpeed: 5000,
+//     });
+// });
 
 // $(function() {
 //     $('.swiper-wrapper').slick({
@@ -15,15 +39,15 @@ $(function() {
 //     });
 // });
 
-$(function(){
-  $(".swiper-wrapper").slick({
-    autoplay: true,
-    autoplaySpeed: 2500,
-    speed: 800,
-    dots: true,
-    arrows: false,
-    centerMode: true,
-    centerPadding: '10%'
-  });
-});
+// $(window).load(function() {
+//   $(".swiper-wrapper").slick({
+//     autoplay: true,
+//     autoplaySpeed: 2500,
+//     speed: 800,
+//     dots: true,
+//     arrows: false,
+//     centerMode: true,
+//     centerPadding: '20%'
+//   });
+// });
 
